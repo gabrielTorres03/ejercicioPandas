67f3f0ec251cf0a9409fa57d4714648e33102468
Gabriel Torres <gtorres22@ujap.edu.ve>
2024-11-28 09:42:42 -0400

    Update codigoCorregido.py

--- codigoCorregido.py
+++ codigoCorregido.py
@@ -1,34 +1,35 @@
 import pandas as pd
 import numpy as np
+
+
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-# Carga de datos
+
 model = pd.read_csv(
-    r"model.txt", sep=r'\s+', skiprows=3, index_col=False)
+    r"model.txt", sep='\s+', skiprows = 3,
+    parse_dates = {'Timestamp': [0, 1]}, index_col = 'Timestamp')
 
-# Crear columna de timestamp y establecer como índice
-model['Timestamp'] = pd.to_datetime(model.iloc[:, 0] + ' ' + model.iloc[:, 1])
-model.set_index('Timestamp', inplace=True)
+model.head()
 
-# Primeras filas del dataset
-print(model.head())
-
-# Matriz de dispersión
 pd.plotting.scatter_matrix(model.loc[model.index[:1000], 'M(m/s)':'D(deg)'])
-pd.plotting.scatter_matrix(
-    model.loc[model.sort_values('M(m/s)', ascending=False).index[:1000], 'M(m/s)':'D(deg)'])
+pd.plotting.scatter_matrix(model.loc[model.sort_values('M(m/s)', ascending=False).index[:1000],'M(m/s)':'D(deg)'])
 
-# Histograma
 model.loc[:, 'M(m/s)'].plot.hist(bins=np.arange(0, 35))
-
-# Agregar columnas de mes y año
 model['month'] = model.index.month
 model['year'] = model.index.year
 
-# Promedio agrupado
-monthly = model.groupby(by=['year', 'month']).mean()
-monthly['ma'] = monthly['M(m/s)'].rolling(5, center=True).mean()
+model.groupby(by = ['year', 'month']).mean().head(24)
 
-# Gráficos
+model.groupby(by=['year', 'month']).mean().plot(y='M(m/s)', figsize=(15, 5))
+
+monthly = model.groupby(by=['year', 'month']).mean()
+monthly['ma'] = monthly.loc[:, 'M(m/s)'].rolling(5, center=True).mean()
+monthly.head()
+
+
 monthly.loc[:, ['M(m/s)', 'ma']].plot(figsize=(15, 6))
+
+monthly.loc[:, 'M(m/s)'].reset_index().pivot(index='year', columns='month')
+
 monthly.loc[:, 'M(m/s)'].reset_index().pivot(index='year', columns='month').T.loc['M(m/s)'].plot(figsize=(15, 5), legend=False)
